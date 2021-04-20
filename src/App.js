7124c7b36cbcb08a464b1710e652d19091e68203
Evan Haralampoudis <e.haralampoudis@gmail.com>
2021-04-19 20:35:14 -0400

    added bootstrap messing around w some stuff

--- src/App.js
+++ src/App.js
@@ -1,56 +1,48 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import classNames from "classnames";
 import React from 'react';
 import { useEffect, useState } from 'react';
 import './App.css';
+import ParticlesBg from "particles-bg";
+import Button from 'react-bootstrap/Button';
+
+
 
 function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
+  function isActive(path) {
+    let pname = window.location.pathname;
+    if(pname === path){
+      return(true);
     }
-    getDate();
-  }, []);
+  };
   return (
-    <main>
-      <h1>Create React App + Go API</h1>
-      <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
-      </h2>
-      <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/master/examples/create-react-app"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        was bootstrapped with{' '}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
-        <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
-      </p>
-      <br />
-      <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
-    </main>
+    <div className="wrapper">
+    <div className="nav">
+      <a href="/a" className={classNames({
+        'nav-item': true,
+        active: isActive('/a'),
+      })}>
+        Link A
+      </a>
+      <a href="/b" className={classNames({
+        'nav-item': true,
+        active: isActive('/b'),
+      })}>
+        Link B
+      </a>
+      <a href="/c" className={classNames({
+        'nav-item': true,
+        active: isActive('/c'),
+      })}>
+        Link C
+      </a>
+    </div>
+    <div classname="background">
+      <Button variant="outline-danger" active>Primary</Button>{' '}
+      <ParticlesBg type="circle" bg={true} />
+    </div>
+</div>
+
   );
 }
 
---
+++ src/components/Background/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from './node_modules/react'
+import ParticlesBg from 'particles-bg'
+
+class background extends Component {
+  render () {
+    return (
+      <ParticlesBg type="circle" bg={true} />
+    )
+  }
+}
+
