9ccd7f32b55b381bd28b3774b527efb5d319a19c
Evan Haralampoudis <e.haralampoudis@gmail.com>
2021-05-13 10:16:54 -0400

    updated nav and background, some messing around w SPA

--- src/App.js
+++ src/App.js
@@ -1,47 +1,22 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { Nav } from './components/Nav';
+import { useEffect, useState } from 'react';
 import classNames from "classnames";
 import React from 'react';
-import { useEffect, useState } from 'react';
-import './App.css';
 import ParticlesBg from "particles-bg";
-import Button from 'react-bootstrap/Button';
+
 
 
 
 function App() {
-  function isActive(path) {
-    let pname = window.location.pathname;
-    if(pname === path){
-      return(true);
-    }
-  };
   return (
     <div className="wrapper">
-    <div className="nav">
-      <a href="/a" className={classNames({
-        'nav-item': true,
-        active: isActive('/a'),
-      })}>
-        Link A
-      </a>
-      <a href="/b" className={classNames({
-        'nav-item': true,
-        active: isActive('/b'),
-      })}>
-        Link B
-      </a>
-      <a href="/c" className={classNames({
-        'nav-item': true,
-        active: isActive('/c'),
-      })}>
-        Link C
-      </a>
+      <Nav />
+      <div classname="background">
+        <ParticlesBg type="square" bg={true} />
+      </div>
     </div>
-    <div classname="background">
-      <Button variant="outline-danger" active>Primary</Button>{' '}
-      <ParticlesBg type="circle" bg={true} />
-    </div>
-</div>
 
   );
 }
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,2 +1,40 @@
-const Nav = () => {};
-export default Nav;
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import classNames from "classnames";
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+
+
+
+export const Nav = () => {
+    function isActive(path) {
+        let pname = window.location.pathname;
+        if(pname === path){
+          return(true);
+        }
+      };
+    return(
+    <div className="nav">
+        <a href="/a" className={classNames({
+        'nav-item': true,
+        active: isActive('/a'),
+      })}>
+        Home
+      </a>
+      <a href="/b" className={classNames({
+        'nav-item': true,
+        active: isActive('/b'),
+      })}>
+        Personal
+      </a>
+      <a href="/c" className={classNames({
+        'nav-item': true,
+        active: isActive('/c'),
+      })}>
+       Memeing
+      </a>
+    </div>
+    )
+}
